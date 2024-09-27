84a6e1b4c55d29afbf3287633a03e7d5fed8ee14
Andres U <urizandres@estudiantec.cr>
2024-09-27 16:15:27 -0600

    Rest API basico

---
+++ Proyecto1/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace Proyecto1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Lista de productos:");
+        }
+
+        [HttpPost]
+        public IActionResult Post(JObject payload)
+        {
+            return Ok(payload);
+        }
+    }
+}
